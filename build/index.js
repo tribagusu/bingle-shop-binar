8ffa1330f34e975660ad8c954d12c2aee518e176
tribagus <tribagus.indonesia@gmail.com>
2023-02-04 13:15:39 +0700

    remove build folder

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./src/app");
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-const port = process.env.PORT;
-app_1.app.listen(port, () => {
-    console.log(`running on port ${port}`);
-});
