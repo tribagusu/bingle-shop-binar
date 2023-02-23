8edf6e0b3641bc15b0abb138bddfa4cc163433dd
tribagus <tribagus.indonesia@gmail.com>
2023-02-23 21:23:37 +0700

    create db uri

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,22 @@
-const dotenv = require("dotenv")
-dotenv.config({ path: ".env" })
+const dotenv = require('dotenv');
+const { Sequelize } = require('sequelize');
+dotenv.config({ path: '.env' });
 
-module.exports = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-  port: parseInt(process.env.DB_PORT, 10),
-  seederStorage: "sequelize",
-  seederStorageTableName: "SequelizeData",
-}
+// module.exports = {
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   host: process.env.DB_HOST,
+//   dialect: process.env.DB_DIALECT,
+//   port: parseInt(process.env.DB_PORT, 10),
+//   seederStorage: "sequelize",
+//   seederStorageTableName: "SequelizeData",
+// }
+
+const db = new Sequelize(DB_URI, {
+  define: {
+    timestamps: false,
+  },
+});
+
+module.exports = db;
