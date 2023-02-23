5eb5cd767a0be66bdb668cd87cbcacc55d972192
tribagus <tribagus.indonesia@gmail.com>
2023-02-23 21:47:28 +0700

    update setup for deployment

--- src/config/database.js
+++ src/config/database.js
@@ -2,21 +2,13 @@ const dotenv = require('dotenv');
 const { Sequelize } = require('sequelize');
 dotenv.config({ path: '.env' });
 
-// module.exports = {
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME,
-//   host: process.env.DB_HOST,
-//   dialect: process.env.DB_DIALECT,
-//   port: parseInt(process.env.DB_PORT, 10),
-//   seederStorage: "sequelize",
-//   seederStorageTableName: "SequelizeData",
-// }
-
-const db = new Sequelize(DB_URI, {
-  define: {
-    timestamps: false,
-  },
-});
-
-module.exports = db;
+module.exports = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_URI,
+  dialect: process.env.DB_DIALECT,
+  port: parseInt(process.env.DB_PORT, 10),
+  seederStorage: 'sequelize',
+  seederStorageTableName: 'SequelizeData',
+};
